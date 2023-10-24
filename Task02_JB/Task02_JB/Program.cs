7565c1647c85a3ce7a1de9cde017fac7ece192cc
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-10-24 12:16:49 +0300

    Removed old files
    Started a new console project

--- Task02_JB/Task02_JB/Program.cs
+++ Task02_JB/Task02_JB/Program.cs
@@ -14,40 +14,47 @@ public class Dog
         string foodAmount;
         if (weight < 5.6)
         {
-            foodAmount = "0,3 to 1"
+            foodAmount = "0,3 to 1";
         }
         else if (weight < 9.1)
         {
-            foodAmount = "1 to 1,3"
+            foodAmount = "1 to 1,3";
         }
         else if (weight < 16)
         {
-            foodAmount = "1,3 to 2"
+            foodAmount = "1,3 to 2";
         }
         else if (weight < 22.6)
         {
-            foodAmount = "2 to 2,6"
+            foodAmount = "2 to 2,6";
         }
         else if (weight < 34.1)
         {
-            foodAmount = "2,6 to 3,3"
+            foodAmount = "2,6 to 3,3";
         }
         else if (weight <= 45)
         {
-            foodAmount = "3,3 to 4,25"
+            foodAmount = "3,3 to 4,25";
         }
         else
         {
-            foodAmount = "WIP"
+            foodAmount = "WIP";
         }
 
         return foodAmount;
     }
-        
+
 }
 
-static void Main(string[] args)
+namespace MyApp
 {
-    Dog tuffy = new Dog(12);
-    Console.WriteLine(tuffy.getFoodAmount());
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog taffy = new Dog(46);
+            Console.WriteLine(taffy.getFoodAmount());
+        }
+    }
 }
+
---
+++ dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Dog
+{
+    float weight;
+
+    public Dog(float weight)
+    {
+        this.weight = weight;
+    }
+
+    public string getFoodAmount()
+    {
+        string foodAmount;
+        if (weight < 5.6)
+        {
+            foodAmount = "0,3 to 1"
+        }
+        else if (weight < 9.1)
+        {
+            foodAmount = "1 to 1,3"
+        }
+        else if (weight < 16)
+        {
+            foodAmount = "1,3 to 2"
+        }
+        else if (weight < 22.6)
+        {
+            foodAmount = "2 to 2,6"
+        }
+        else if (weight < 34.1)
+        {
+            foodAmount = "2,6 to 3,3"
+        }
+        else if (weight <= 45)
+        {
+            foodAmount = "3,3 to 4,25"
+        }
+        else
+        {
+            foodAmount = "WIP"
+        }
+
+        return foodAmount;
+    }
+
+}
+
