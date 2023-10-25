f1d6aab9c386b9b4e9c14616d43c2dbb87f90b0a
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-10-25 11:26:46 +0300

    Squashed commit of the following:
    commit 612da407483e732d5c725b457aa50e4747b15763
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 25 11:15:39 2023 +0300
    
        Cosmetic changes
    
    commit 001a9883ebced2b1f30979950a8c4874236c6aed
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 25 10:57:23 2023 +0300
    
        Added exception handling
    
        Throws an error when input is invalid
    
    commit b3c282c2ae8c48b922a639cb07baea45155e9d65
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 25 10:44:25 2023 +0300
    
        Added basic functionality
    
        Can type dog weight in text box and it returns food amount
    
    commit f8cdb697e21828fb0146747777d36e0c154a9de7
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 25 10:31:45 2023 +0300
    
        Added button and text box
    
    commit e45fa4bb0eb0ce3b1e681f1a5b34b8f4ce0c44dd
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 13:39:13 2023 +0300
    
        Set up form project
    
        Added dog.cs to form project
    
    commit ccd05214ad586f37082bed783f732926bddf8f2c
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 13:30:53 2023 +0300
    
        Renamed variables
    
    commit 8dae7c7016d072a5ab10fc29c1793221e12c2517
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 13:22:09 2023 +0300
    
        Added +45 category
    
    commit 7565c1647c85a3ce7a1de9cde017fac7ece192cc
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 12:16:49 2023 +0300
    
        Removed old files
    
        Started a new console project
    
    commit e4f1499196c395247ffc06d952ab6964847b8afd
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 11:57:34 2023 +0300
    
        Else if structure
    
    commit e1db9d61f66a78453300c124f52e42928b3f93ca
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Oct 24 11:57:24 2023 +0300
    
        Output type to console
    
    commit f2a14abe48ebf8cced388480d882f2bfae14dc20
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 11 11:24:57 2023 +0300
    
        Removed switch statement
    
        Removed switch statement from getFoodAmount()
    
    commit 3e10ae936fc32b66f137df459fd9bccfa833ad5f
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Wed Oct 11 10:56:28 2023 +0300
    
        Created Task01 files
    
        Windows forms app + dog class

---
+++ Task02-form_JB/Task02-form_JB/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Task02_form_JB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.dogWeightLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox.Location = new System.Drawing.Point(135, 98);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(115, 26);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "Enter weight";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(135, 142);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(115, 42);
+            this.button.TabIndex = 1;
+            this.button.Text = "Calculate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // dogWeightLabel
+            // 
+            this.dogWeightLabel.AutoSize = true;
+            this.dogWeightLabel.Location = new System.Drawing.Point(131, 75);
+            this.dogWeightLabel.Name = "dogWeightLabel";
+            this.dogWeightLabel.Size = new System.Drawing.Size(120, 20);
+            this.dogWeightLabel.TabIndex = 2;
+            this.dogWeightLabel.Text = "Dog weight (kg)";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.MaximumSize = new System.Drawing.Size(400, 0);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(368, 40);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "This tool calculates how many cups of dry food you should feed your dog per day.";
+            this.infoLabel.Click += new System.EventHandler(this.infoLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 202);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.dogWeightLabel);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(412, 258);
+            this.MinimumSize = new System.Drawing.Size(412, 258);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label dogWeightLabel;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ Task02-form_JB/Task02-form_JB/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Task02_form_JB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            textBox.ForeColor = Color.Black;
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string userInput = textBox.Text;
+            bool conversionSuccess = float.TryParse(userInput, out float dogWeight);
+
+            if (conversionSuccess && dogWeight > 0) 
+            {
+                Dog yourDog = new Dog(dogWeight);
+                MessageBox.Show($"Recommended food amount: {yourDog.getFoodAmount()} cups per day");
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void infoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
