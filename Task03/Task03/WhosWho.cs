60ababb2618a0ebd95bebc4f4787b60b60c71df0
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-11-21 12:37:31 +0200

    Squashed commit of the following:
    commit 028de31f3240b351d190f3ea153bb42d6296f125
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Nov 21 12:36:36 2023 +0200
    
        Finished Task03
    
    commit 964d77fc12c3ee4ce4cd02fa3eea1f4f760e86b0
    Author: jul18ra <145108891+jul18ra@users.noreply.github.com>
    Date:   Tue Nov 21 12:30:26 2023 +0200
    
        Added all required classes

---
+++ Task03/Task03/WhosWho.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class WhosWho
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person();
+            peter.Greet();
+
+            Student mary = new Student();
+            mary.SetAge(12);
+            mary.Greet();
+            mary.ShowAge();
+            mary.Study();
+
+            Teacher james = new Teacher();
+            james.SetAge(36);
+            james.Greet();
+            james.Explain();
+        }
+    }
+}
