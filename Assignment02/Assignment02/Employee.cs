ecef98b43406ce4182910b09d99cc39b5756615a
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-11-21 13:03:55 +0200

    Finished Employee class

--- Assignment02/Assignment02/Employee.cs
+++ Assignment02/Assignment02/Employee.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Assignment02
 {
     internal class Employee : Person
     {
+        private string companyName;
+        private int employeeNumber;
+
+        public Employee(string companyName, int employeeNumber) : base("", "")
+        {
+            this.companyName = companyName;
+            this.employeeNumber = employeeNumber;  
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                return companyName;
+            }
+
+            set
+            {
+                companyName = value;
+            }
+
+        }
+
+        public int EmployeeNumber
+        {
+            get
+            {
+                return employeeNumber;
+            }
+
+            set
+            {
+                employeeNumber = value;
+            }
+
+        }
     }
 }
