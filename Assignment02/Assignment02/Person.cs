29d2c1eaaac3d8f5cd5569d967ce5ac31a368cbc
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-11-21 12:57:07 +0200

    Finished Person class

--- Assignment02/Assignment02/Person.cs
+++ Assignment02/Assignment02/Person.cs
@@ -8,5 +8,41 @@ namespace Assignment02
 {
     internal class Person
     {
+        string name;
+        string address;
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+
+        }
     }
 }
