257c8027d6a9dffa6093d7ac19235212680ce3e5
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 22:58:15 +0200

    Copied Task04B base

---
+++ Task04/Task04/Task04B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    public class Task04B
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Student newStudent = null;
+                try
+                {
+                    newStudent = new Student();
+                    newStudent.StudentID = "James007";
+                    ValidateStudent(newStudent);
+                }
+                catch (InvalidStudentNameExceptionex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
