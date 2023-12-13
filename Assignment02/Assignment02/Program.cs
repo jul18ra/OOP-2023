087b64944666f88ade0b1f0f9348685731c84f1d
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 21:55:33 +0200

    Cleanup
    - Small fixes

--- Assignment02/Assignment02/Program.cs
+++ Assignment02/Assignment02/Program.cs
@@ -9,15 +9,22 @@ namespace Assignment02
 {
     internal class Program
     {
+        private string name = "";
+        private string address = "";
+        private string companyName = "";
+        private string employeeCategory = "";
+        private int employeeNumber = 0;
+        private bool employeeNumberIsValid;
+        private int salary = 0;
+
         static void Main(string[] args)
         {
-            string name;
-            string address;
-            string companyName;
-            string employeeCategory = "";
-            int employeeNumber = 0;
-            int salary = 0;
+            Program program = new Program();
+            program.CreateEmployee();
+        }
 
+        private void AskForInfo()
+        {
             Console.WriteLine("Enter employee name:");
             name = Console.ReadLine() ?? "";
             Console.WriteLine("\n");
@@ -34,21 +41,37 @@ namespace Assignment02
             {
                 Console.WriteLine("Enter employee category (programmer, tester, marketer):");
                 employeeCategory = Console.ReadLine() ?? "programmer";
+
+                if(employeeCategory != "programmer" & employeeCategory != "tester" & employeeCategory != "marketer")
+                {
+                    Console.WriteLine("Invalid category");
+                }
                 Console.WriteLine("\n");
+
             }
 
-            Console.WriteLine("Enter employee number:");
-            try
+            while (!employeeNumberIsValid)
             {
-                employeeNumber = Int32.Parse(Console.ReadLine() ?? "0");
-            }
-            catch
-            {
-                Console.WriteLine("Invalid employee number");
+                Console.WriteLine("Enter employee number:");
+                try
+                {
+                    employeeNumber = Int32.Parse(Console.ReadLine() ?? "0");
+                    employeeNumberIsValid = true;   
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid employee number");
+                    Console.WriteLine("\n");
+
+                }
+
             }
 
-            Console.WriteLine("\n");
 
+        }
+
+        private void PickEmployeeCategory()
+        {
             switch (employeeCategory)
             {
                 case "programmer":
@@ -58,7 +81,7 @@ namespace Assignment02
 
                 case "tester":
                     Tester tester = new Tester(name, address, companyName, employeeNumber);
-                    salary = tester.Salary; 
+                    salary = tester.Salary;
                     break;
 
                 case "marketer":
@@ -67,12 +90,22 @@ namespace Assignment02
                     break;
             }
 
+        }
+
+        private void DisplayInfo()
+        {
             Console.WriteLine($"Employee name: {name}");
             Console.WriteLine($"Employee address: {address}");
             Console.WriteLine($"Company name: {companyName}");
             Console.WriteLine($"Employee number: {employeeNumber}");
             Console.WriteLine($"Employee salary: {salary}");
+        }
 
+        private void CreateEmployee()
+        {
+            AskForInfo();
+            PickEmployeeCategory();
+            DisplayInfo();
         }
 
     }
