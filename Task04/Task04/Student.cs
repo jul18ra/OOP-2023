c2689ea886f6915ec929d80d7668ab274d7101d2
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 23:05:24 +0200

    Added student class

---
+++ Task04/Task04/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Student
+    {
+        private int studentID;
+        public int StudentID { get { return studentId; } set { studentId = value; } }
+
+        Student(int studentID)
+        {
+            this.studentID = studentID;
+        }
+    }
+}
