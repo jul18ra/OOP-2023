a9ee5f6d1ce3ca07508c69f0db1505dedbf2b647
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 20:56:22 +0200

    Class exercises

---
+++ Multithreading/Multithreading/MyProgram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithreading
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int input = Int32.Parse(Console.ReadLine());
+
+            Thread thr1 = new Thread(userTimesTable);
+            Thread thr2 = new Thread(userFactorial);
+
+            thr1.Start(input);
+            thr2.Start(input);
+        }
+
+        static void userTimesTable(object x)
+        {
+            int userInput = Convert.ToInt32(x);
+            for (int n = 1; n <= 10; n++) 
+            { 
+                Console.WriteLine($"{userInput} x {n} = {userInput * n}");
+            }
+        }
+
+        static void userFactorial(object x)
+        {
+            int userInput = Convert.ToInt32(x);
+
+            int n = 1;
+            int answer = 1;
+
+            while (n <= userInput)
+            {
+                int a = answer;
+                answer = answer * n;
+                Console.WriteLine($"{a} x {n} = {answer}");
+
+                n++;
+            }
+        }
+
+    }
+}
---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TestApp
+{
+    class MyException : Exception
+    {
+        public void InvalidDateException() 
+        {
+            Console.WriteLine("Invalid date format");
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter today's date:");
+            input = Console.ReadLine();
+
+            string pattern = @"[0-9]{2} [a-z]{3} [0-9]{4}";
+            Regex rgx = new Regex(pattern);
+
+            try
+            {
+                if (rgx.IsMatch(input))
+                {
+                    Console.WriteLine("Thanks for telling me");
+                }
+                else
+                {
+                    throw new MyException();
+                }
+            }
+
+            catch (MyException ex) 
+            {
+                ex.InvalidDateException();
+            }
+
+
+
+        }
+    }
+}
