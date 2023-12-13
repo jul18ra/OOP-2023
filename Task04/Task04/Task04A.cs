c29fdd9837291d2746584fa7a50acbbb2d6477df
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 22:45:27 +0200

    Copied Task04A base

---
+++ Task04/Task04/Task04A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    public class Task04A
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a number to divide 100: ");
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                int result = 100;
+                Console.WriteLine("100 / {0} = {1}", num, result);
+            }
+        }
+    }
+}
