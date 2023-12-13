ec2ddb6ab69268523f86ac28db433c927af4231f
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-13 22:54:05 +0200

    Finished Task04A

--- Task04/Task04/Task04A.cs
+++ Task04/Task04/Task04A.cs
@@ -10,12 +10,34 @@ namespace Task04
     {
         public static void Main()
         {
-            Console.WriteLine("Please enter a number to divide 100: ");
-            try
+            bool inputIsValid = false;
+
+            while (!inputIsValid)
             {
-                int num = int.Parse(Console.ReadLine());
-                int result = 100;
-                Console.WriteLine("100 / {0} = {1}", num, result);
+                Console.WriteLine("Please enter a number to divide 100: ");
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    int result = 100 / num;
+                    Console.WriteLine("100 / {0} = {1}", num, result);
+                    inputIsValid = true;    
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Can't divide by zero");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input must be of type int");
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Invalid operation");
+                }
+                catch
+                {
+                    Console.WriteLine("An error has occurred");
+                }
             }
         }
     }
