44fe27cbe0f94da4709801aaca3f11dc9ca3e0ce
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-14 22:51:10 +0200

    Implemented age exception handling

--- Assignment03A/Assignment03A/Program.cs
+++ Assignment03A/Assignment03A/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Assignment03A
@@ -13,16 +14,39 @@ namespace Assignment03A
             string name;
             string email;
             int age;
+            bool inputIsValid = false;
 
             Console.WriteLine("Enter full name:");
             name = Console.ReadLine();
 
-            Console.WriteLine("Enter age:");
-            age = int.Parse(Console.ReadLine());
 
+            while(!inputIsValid)
+            {
+                Console.WriteLine("Enter age:");
+
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+                    if (age > 0)
+                    {
+                        inputIsValid = true;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid age");
+                }
+            }
+         
             Console.WriteLine("Enter email address:");
             email = Console.ReadLine();
-
+            
         }
+
     }
 }
