8bb61ccb62acd419c5b178a0626e49e75912edd4
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-14 16:36:40 +0200

    Created Assignment03A base

---
+++ Assignment03A/Assignment03A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string email;
+            int age;
+
+            Console.WriteLine("Enter full name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter age:");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter email address:");
+            email = Console.ReadLine();
+
+        }
+    }
+}
