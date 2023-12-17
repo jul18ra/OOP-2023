44fb3866b2d255b7462c5a69784883b320eabe10
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-17 14:54:59 +0200

    Commented code

--- Assignment04/Assignment04/ObjOrPro.cs
+++ Assignment04/Assignment04/ObjOrPro.cs
@@ -1,35 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment04
+﻿
+class ObjOrPro
 {
-    class ObjOrPro
+    // A list of max 10 threads
+    static Thread[] threads = new Thread[10];
+
+    // A semaphore with an initial count of 3 and max count of 3
+    static Semaphore sem = new Semaphore(3, 3);
+
+    static void SharedResource()
     {
-        static Thread[] threads = new Thread[10];
-        static Semaphore sem = new Semaphore(3, 3);
+        // Thread requests semaphore
+        Console.WriteLine("{0} is waiting in line...", Thread.CurrentThread.Name);
+        sem.WaitOne();
 
-        static void SharedResource()
-        {
-            Console.WriteLine("{0} is waiting in line...", Thread.CurrentThread.Name);
-            sem.WaitOne();
-            Console.WriteLine("{0} enters the shared resource!", Thread.CurrentThread.Name);
-            Thread.Sleep(300);
-            Console.WriteLine("{0} is leaving the shared resource", Thread.CurrentThread.Name);
-            sem.Release();
-        }
+        // Thread enters resource and is suspended for 300 ms
+        Console.WriteLine("{0} enters the shared resource!", Thread.CurrentThread.Name);
+        Thread.Sleep(300);
 
-        static void Main(string[] args)
+        // Thread leaves resource and releases one lock
+        Console.WriteLine("{0} is leaving the shared resource", Thread.CurrentThread.Name);
+        sem.Release();
+    }
+
+    static void Main(string[] args)
+    {
+        // Creates and starts 10 threads
+        for (int i = 0; i < 10; i++)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                threads[i] = new Thread(SharedResource);
-                threads[i].Name = "thread_" + i;
-                threads[i].Start();
-            }
-            Console.Read();
+            threads[i] = new Thread(SharedResource);
+            threads[i].Name = "thread_" + i;
+            threads[i].Start();
         }
+        Console.Read();
     }
 }
